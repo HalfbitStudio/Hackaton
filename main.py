5afe46980788332cf9d58c2a8a8da25026919242
Marcin Golonka <mer@MacBook-Air-Marcin.local>
2021-11-20 17:43:29 +0100

    api

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import pickle
+import tensorflow as tf
+from PIL import Image
+from io import BytesIO
+import base64
+import numpy as np
+from flask import Flask, request, jsonify
 
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+classess = pickle.load(open('labels.pcl', 'rb'))
+model = tf.keras.models.load_model('model.h5')
+
+app = Flask(__name__)
+
+print(classess)
+
+@app.route("/api/predict", methods=['POST'])
+def predict():
+    data = request.json.get('image')
+    im = np.asarray(Image.open(BytesIO(base64.b64decode(data))).convert('RGB').resize((224,224)))
+    im = tf.expand_dims(im, 0)  # Create a batch
+    predictions = model.predict(im)
+    score = tf.nn.softmax(predictions[0])
+    resp = {"label": classess[np.argmax(score)], "confidence":100 * np.max(score) }
+    print(resp)
+    return resp
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(host='0.0.0.0', port=7777)
